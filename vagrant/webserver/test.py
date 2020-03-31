b818ec87d85a791ff188903764336c99d647ba91
Rigers <rig.tsm@gmail.com>
2020-03-31 23:49:56 +0200

    working web webserver

--- vagrant/webserver/test.py
+++ vagrant/webserver/test.py
@@ -1,63 +1,39 @@
-# Using the BaseHttpServer library
-# I will be using python3 so i will use someting different
-#from http.server import BaseHTTPRequestHandler, HTTPServer # not working
-
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer # Works perfectly for python 2
-
-# The server is devided in two main sections
-######### handler section #########
-# Here we indicate what code to execute based on the type of HTTP request that is sent to the server
-
-# beeing used in the Post
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
-
 
 
 class webServerHandler(BaseHTTPRequestHandler):
 
-    # handle the GET request our server receives and shold figure out which resources are requeste to access
     def do_GET(self):
         try:
-            # try for url localhost:8080/hello, then send the 'output' text response
             if self.path.endswith("/hello"):
-
-                self.send_response(200) # successful GET request
-                self.send_header('Cntent-type', 'text/html') # inform the client i will reply with a text/html file
-                self.end_headers() # send a blank line to indicate the end of the headers response.
-
-                # now lest send some content to the client
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
                 output = ""
                 output += "<html><body>"
                 output += "<h1>Hello!</h1>"
                 output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
                 output += "</body></html>"
+                self.wfile.write(output)
+                print output
+                return
 
-                self.wfile.write(output) # Send the message back to the client
-                print(output) # just check the output on the console / debuggin porpuse
-                return # exit
-
-            # try for url localhost:8080/hello, then send the 'output' text response
             if self.path.endswith("/hola"):
-
-                self.send_response(200) # successful GET request
-                self.send_header('Cntent-type', 'text/html') # inform the client i will reply with a text/html file
-                self.end_headers() # send a blank line to indicate the end of the headers response.
-
-                # now lest send some content to the client
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
                 output = ""
                 output += "<html><body>"
                 output += "<h1>&#161 Hola !</h1>"
                 output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
                 output += "</body></html>"
+                self.wfile.write(output)
+                print output
+                return
 
-                self.wfile.write(output) # Send the message back to the client
-                print(output) # just check the output on the console / debuggin porpuse
-                return # exit
-
-        except IOError as error:
-            print("File not found %s" % self.path)
-
-
+        except IOError:
+            self.send_error(404, 'File Not Found: %s' % self.path)
 
     def do_POST(self):
         try:
@@ -69,7 +45,6 @@ class webServerHandler(BaseHTTPRequestHandler):
             if ctype == 'multipart/form-data':
                 fields = cgi.parse_multipart(self.rfile, pdict)
                 messagecontent = fields.get('message')
-
             output = ""
             output += "<html><body>"
             output += " <h2> Okay, how about this: </h2>"
@@ -77,18 +52,11 @@ class webServerHandler(BaseHTTPRequestHandler):
             output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
             output += "</body></html>"
             self.wfile.write(output)
-            print(output)
-
+            print output
         except:
             pass
 
 
-
-
-
-
-######### main section #########
-# We instantiate our server and specify what port it will listen to
 def main():
     try:
         port = 8080
@@ -99,8 +67,5 @@ def main():
         print " ^C entered, stopping web server...."
         server.socket.close()
 
-
-
-# run immediatly the main method when the interpreter executes the script
 if __name__ == '__main__':
     main()
