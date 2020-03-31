7e8c025a5f8d68ae3c59d0ef1fffc604d74279db
Rigers <rig.tsm@gmail.com>
2020-04-01 01:47:08 +0200

    obj2 completed

--- vagrant/webserver/obj2.py
+++ vagrant/webserver/obj2.py
@@ -58,7 +58,13 @@ class webServerHandler(BaseHTTPRequestHandler):
                 restaurants = session.query(Restaurant).all()
                 for restaurant in restaurants:
                     output += restaurant.name
-                    output += "</br></br>"
+                    output += "</br>"
+                    # Objective 2 -- Add Edit and Delete Links
+                    output += "<a href ='#' > Edit </a> "
+                    output += "</br>"
+                    output += "<a href =' #'> Delete </a>"
+                    output += "</br></br></br>"
+
                 output += "</body></html>"
 
                 self.wfile.write(output) # Send the message back to the client
